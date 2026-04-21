fb10d4308b375aa396f30c0e767eb4a4e4ed8b55
Warintorn Chomsang <warintorn.chomsang@intellity.co.th>
2026-04-21 12:01:03 +0700

    [new] helper: add connect.ps1 for connect to azure

---
+++ helper/connect.ps1
@@ -0,0 +1,33 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [switch]
+    $disconnect
+)
+
+if ($disconnect) {
+    Write-Host "🔌 Disconnecting from Azure..." -ForegroundColor Yellow
+    Disconnect-AzAccount -Confirm:$false
+    Write-Host "✅ Successfully disconnected from Azure!" -ForegroundColor Green
+    exit 0
+}
+
+# Verify if there's an active Azure session
+$context = Get-AzContext -ErrorAction SilentlyContinue
+
+if ($null -eq $context) {
+    Write-Host "🔐 No active session found. Initiating secure WAM login..." -ForegroundColor Yellow
+
+    Connect-AzAccount
+    $context = Get-AzContext
+
+    Write-Host "✅ Successfully connected to Azure!" -ForegroundColor Green
+}
+else {
+    Write-Host "✅ Active session detected. Using existing context..." -ForegroundColor Green
+    Write-Host "---------------------------------------------------" -ForegroundColor Cyan
+    Write-Host "Connected to : $($context.Subscription.Name)"        -ForegroundColor Green
+    Write-Host "Account      : $($context.Account.Id)"
+    Write-Host "Tenant       : $($context.Tenant.Id)"
+    Write-Host "---------------------------------------------------" -ForegroundColor Cyan
+}
